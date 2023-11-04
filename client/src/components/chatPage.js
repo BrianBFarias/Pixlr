0c454b4ad8833f95af1120ffa23795f41d46bc51
Brian Farias <Brian03032003@gmail.com>
2023-11-04 12:11:16 -0400

    updated graphics

--- client/src/components/chatPage.js
+++ client/src/components/chatPage.js
@@ -11,14 +11,18 @@ const random3 = randomValueInRange(0.2, 1.8);
 const random4 = randomValueInRange(0.2, 1.8);
 
 function ChatPage() {
-  
+  var i=0;
+  const speed = 40;
+
+  const box = document.getElementById("output");
+
   document.documentElement.style.setProperty('--random1', random1);
   document.documentElement.style.setProperty('--random2', random2);
   document.documentElement.style.setProperty('--random3', random3);
   document.documentElement.style.setProperty('--random4', random4);
 
   const [inputValue, setInputValue] = useState(""); // State to hold the input value
-  const [imageSrc, setImageSrc] = useState(""); // State to hold the image source
+  const [text, setText] = useState(""); // State to hold the image source
   const [loading, setLoading] = useState(false); // State to track loading status
 
 
@@ -26,42 +30,47 @@ function ChatPage() {
     setInputValue(e.target.value); // Update the input value when it changes
   };
 
+  function typeWriter() {
+    if (i < inputValue.length) {
+      box.value += inputValue.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     if(inputValue != null){
+      i=0;
       console.log(inputValue)
-      setImageSrc(null);
+      // setText(null);
       setLoading(true);
       const stringInput  = inputValue;
       setInputValue("");
-  
+      box.value = ""
       const inputElement = document.querySelector('.sb input');
       inputElement.disabled = true;
+
+      // const response = await fetch("http://localhost:8080/detail", {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify({data: `${stringInput}`})
+      // });
   
-      try {
-        const response = await fetch("http://localhost:8080/detail", {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({data: `${stringInput}`})
-        });
-    
-        const data = await response.json();
-        setImageSrc(data.message[0].url);
-      } catch (error) {
-        console.error('Error fetching or parsing data:', error);
-      } finally {
-        setLoading(false); // Set loading back to false
-        inputElement.disabled = false;
-      }
+      // const data = await response.json();
+      setLoading(false); // Set loading back to false
+      await typeWriter()
+      inputElement.disabled = false;
     }
   }
 
   return (
     <div className="body">
       <main>
-      <textarea disabled className="target" name="output" id="" cols="30" rows="10" value={""}></textarea>
+      <textarea disabled id="output" className="target" name="output" cols="30" rows="10"></textarea>
         <div className="sin-nav">
           <form onSubmit={handleSubmit} className="sb">
             <div className="rsb">
