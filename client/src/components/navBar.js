c351a16e5644a89b09255497e63e74a5f475ada6
Brian Farias <Brian03032003@gmail.com>
2023-11-06 11:22:52 -0500

    updated selection

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -5,31 +5,32 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
 
-  const selected = async (e) => {
-    const unselected = document.querySelector('.nav-links');
-    const selected = document.querySelector('.nav-links-sel');
-    unselected.className = 'nav-links-sel';
-    selected.className = 'nav-links';
+  const [selectedLink, setSelectedLink] = useState('pic');
+
+  const handleLinkClick = (id) => {
+    setSelectedLink(id);
   }
 
   return (
     <>
       <nav className='navbar'>
         <div className='options'>
-            <Link
-                to='/'
-                className='nav-links-sel'
-                onClick={selected}
-              >
-                <h1>PicBot</h1>
-              </Link>
-              <Link
-                to='/chat'
-                className='nav-links'
-                onClick={selected}
-              >
-                <h1>ChatBot</h1>
-              </Link>
+          <Link
+            to='/'
+            id='pic'
+            className={selectedLink === 'pic' ? 'nav-links-sel' : 'nav-links'}
+            onClick={() => handleLinkClick('pic')}
+          >
+            <h1>PicBot</h1>
+          </Link>
+          <Link
+            to='/chat'
+            id='chat'
+            className={selectedLink === 'chat' ? 'nav-links-sel' : 'nav-links'}
+            onClick={() => handleLinkClick('chat')}
+          >
+            <h1>ChatBot</h1>
+          </Link>
         </div>
       </nav>
     </>
